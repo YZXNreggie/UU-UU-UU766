88c2059e80514aa28dc81d476fcfaf0699328016
reggiexxx <62551220+YZXNreggie@users.noreply.github.com>
2021-11-13 23:05:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git lean https://github.com/coolsnowwolf/lede/tree/master/package/lean' >>feeds.conf.default
+#echo 'src-git lean https://github.com/coolsnowwolf/lede/tree/master/package/lean' >>feeds.conf.default
