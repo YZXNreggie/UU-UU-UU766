8711c0ab58e3082e6ed749ebadd4b1708bf6589a
reggiexxx <62551220+YZXNreggie@users.noreply.github.com>
2021-11-19 20:45:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
  #Add a feed source
-echo 'src-git packages https://github.com/immortalwrt/packages.git;openwrt-18.06' >>feeds.conf.default
+#echo 'src-git packages https://github.com/immortalwrt/packages.git;openwrt-18.06' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git luci https://github.com/immortalwrt/luci.git;openwrt-18.06' >>feeds.conf.default
-echo 'src-git package https://github.com/Droid-MAX/luci-app-lingtigamebooster.git'>feeds.conf.default
-echo 'src-git routing https://github.com/immortalwrt/routing.git;openwrt-18.06'>feeds.conf.default
-echo 'src-git telephony https://github.com/openwrt/telephony.git'>feeds.conf.default
+#echo 'src-git luci https://github.com/immortalwrt/luci.git;openwrt-18.06' >>feeds.conf.default
+#echo 'src-git package https://github.com/Droid-MAX/luci-app-lingtigamebooster.git'>feeds.conf.default
+#echo 'src-git routing https://github.com/immortalwrt/routing.git;openwrt-18.06'>feeds.conf.default
+#echo 'src-git telephony https://github.com/openwrt/telephony.git'>feeds.conf.default
